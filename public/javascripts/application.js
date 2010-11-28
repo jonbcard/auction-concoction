8978fa8e187365e015585972abe06252b9f2b52e
Jon Card <jon.card@gmail.com>
2010-11-27 18:53:47 -0700

    Upgrading to knockoutjs to 1.1.1. Updating filter method.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,25 @@
+String.prototype.startsWith = function(str){
+    // TODO: kind of slow implementation. It would be nice
+    // to have this check as lightening fast as possible.
+    return (this.indexOf(str) === 0);
+}
+
 $(document).ready(function(){
-	// Setup for input fields
-	$('input[data-type|="date"]').datepicker();
+    // Setup for input fields
+    $('input[data-type|="date"]').datepicker();
 	
-	// Setup for confirmation dialogs
-	$('form[data-confirm]').live('click', function(e) {
-		e.preventDefault();
-	  $('#dialog').dialog({
-			resizable : false,
-			modal : true,
-		});
-		$("#dialog").dialog("open");
-		return false;
-	  if (!confirm($(this).attr('data-confirm'))) {
-	    return false;
-	  }
-	});
+    // Setup for confirmation dialogs
+    $('form[data-confirm]').live('click', function(e) {
+        e.preventDefault();
+        $('#dialog').dialog({
+            resizable : false,
+            modal : true
+        });
+        $("#dialog").dialog("open");
+        return false;
+        if (!confirm($(this).attr('data-confirm'))) {
+            return false;
+        }
+    });
 	
 });
