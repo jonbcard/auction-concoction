bedf4f09e56232e1b237a5fa93042fe6c1d841d2
Jon Card <jon.card@gmail.com>
2010-12-09 19:47:42 -0800

    Cleaned up/fixed confirmation dialog javascript.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,27 @@
-// TODO : Organize this file a little better
+// ---- Stuff we always want to do on load (UJS sort of stuff)  ----
+
+$(document).ready(function(){
+    // Setup for input fields
+    $('input[data-type|="date"]').datepicker();
+    $("form[data-confirm]").each(function(){
+        // TODO : The same dialog is re-initialized many times if the confirmation
+        // is in a table. This is bad because 1) it wipes out the dialog text, if
+        // it is different, and 2) its wasteful.
+        setupConfirmationDialog($("#dialog"), $(this), $(this).attr("data-confirm"));
+    });
+
+});
+
+// ---- General helper functions ----
 
 String.prototype.startsWith = function(str){
     // TODO: kind of slow implementation. It would be nice
     // to have this check as lightening fast as possible.
     return (this.indexOf(str) === 0);
+}
+
+var nullOrEmpty = function(value){
+    return value == null || value.length == 0
 }
 
 /**
@@ -19,6 +37,36 @@ var sortArrayByProperty = function(array, property){
     });
 }
 
+// ---- JQuery helper stuff ----
+setupConfirmationDialog = function(dialogHandle, domHandle, dialogText){
+    // Setup the options on the dialog
+    dialogHandle.dialog({
+        modal:true,
+        closeOnEscape:false,
+        autoOpen:false,
+        resizable:false,
+        buttons : {
+            "Confirm" : function() {
+                $(this).dialog("close");
+                domHandle.get()[0].submit();
+            },
+            "Cancel" : function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+
+    domHandle.bind("submit", function(e){
+        $("#dialog").html(dialogText);
+        $("#dialog").dialog('open');
+        return false;
+    });
+
+    domHandle.click(function(e){
+        e.stopPropagation();
+    });
+}
+
 var submitOnEnter = function(formId){
     $('input').keypress(function(e){
         if(e.which == 13){
@@ -27,89 +75,13 @@ var submitOnEnter = function(formId){
     });
 }
 
-
-
-$(document).ready(function(){
-    // Setup for input fields
-    $('input[data-type|="date"]').datepicker();
-
-    // Setup any confirmation dialogs
-    $("#dialog").dialog({
-        modal:true,
-        closeOnEscape:false,
-        autoOpen:false
-    });
-
-    // Stop events from propogating further when we're pulling up
-    // a confirmation dialog.
-    $("form[data-confirm]").click(function(e){
-        e.stopPropagation();
-        return true;
-    });
-
-    // Little hack to get the popup dialog working for now
-    var confirmed = false;
-    $("form[data-confirm]").submit(function(e){
-        var dialogText = $(this).attr("data-confirm");
-
-         var form = $(this);
-        $("#dialog").html(dialogText);
-        $("#dialog").dialog({
-            buttons : {
-                "Confirm" : function() {
-                    confirmed = true;
-                    form.submit();
-                },
-                "Cancel" : function() {
-                    $(this).dialog("close");
-
-                }
-            }
-        });
-        $("#dialog").dialog('open');
-        return confirmed;
-    });
-    	
-});
-
-// ---- Custom knockout bindings ----
-
-// TODO - This is broken when used in a table for some reason..
-// TODO - Cleanup code to not duplicate the data-confirm stuff..
+// ---- Custom Knockout JS bindings ----
 ko.bindingHandlers.confirm = {
     init: function(element, valueAccessor, allBindingsAccessor, viewModel) {
-        var value = valueAccessor();
-        var valueUnwrapped = ko.utils.unwrapObservable(value);
-        var confirmed = false;
-
-        $(element).click(function(e){
-            e.stopPropagation();
-            return true;
-        });
-
-        $(element).submit(function(e){
-            if(!confirmed){
-                var form = $(element);
-                $("#dialog").html(valueUnwrapped);
-                $("#dialog").dialog({
-                    buttons : {
-                        "Confirm" : function() {
-                            confirmed = true;
-                            form.submit();
-                        },
-                        "Cancel" : function() {
-                            $(this).dialog("close");
-                        }
-                    }
-                });
-                $("#dialog").dialog('open');
-            }
-            return confirmed;
-        });
+        var valueUnwrapped = ko.utils.unwrapObservable(valueAccessor());
+        setupConfirmationDialog($("#dialog"), $(element), valueUnwrapped);
     },
     update: function(element, valueAccessor, allBindingsAccessor, viewModel) {
-        var value = valueAccessor();
-        var valueUnwrapped = ko.utils.unwrapObservable(value);
-        $("#dialog").html(valueUnwrapped);
+        // NO-OP - for now, no support for dynamic dialog text.
     }
 }
