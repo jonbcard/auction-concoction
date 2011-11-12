60989108b24c23f1ad8be49d4281d4a4704a24c0
Jon Card <jon.card@gmail.com>
2011-11-11 21:22:17 -0700

    Adding Growl-style messages throughout

--- app/models/consignee.rb
+++ app/models/consignee.rb
@@ -4,13 +4,19 @@ class Consignee
   # Scopes
   scope :by_code,  lambda { |code| where(:code => code) }
   
-  # Keys
+  # General keys
   key :code,             String     # Alpha-numeric code for the location
   key :name,             String     # Display name for the consignee
   key :address,          String     
   key :city,             String
   key :state,            String
   key :external_code,    String     # Link to external accounting system
+  
+  # Primary contact keys
+  key :first_name,       String     # First name of the primary contact
+  key :last_name,        String     # Last name of the primary contact
+  key :phone,            String     # Phone of the primary contact
+  key :phone_ext,        String     # Phone extension (these still exist, right?)
 
   # Validations
   validates_length_of       :code,    :within => 3..16
