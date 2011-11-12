7d2158a563e406c751f2b9940d17bad35c35869a
Jon Card <jon.card@gmail.com>
2011-11-11 18:56:03 -0700

    Adding search feature for customers

--- app/controllers/customers.rb
+++ app/controllers/customers.rb
@@ -12,9 +12,21 @@ AuctionNow.controllers :customers do
   get :index, :with => :id, :provides => :json do
     return Customer.find(params[:id]).to_json
   end
+  
+  post :search, :provides => [:json] do
+    criteria = parse_json(request)
+    query_params = {}
+    query_params[:id_number] = criteria["id_number"] unless criteria["id_number"].blank?
+    query_params[:bidder_number] = criteria["bidder_number"] unless criteria["bidder_number"].blank?
+    query_params[:first_name] = /^#{criteria["first_name"]}/i unless criteria["first_name"].blank?
+    query_params[:last_name] = /^#{criteria["last_name"]}/i unless criteria["last_name"].blank?
+    query_params[:email] = /^#{criteria["email"]}/i unless criteria["email"].blank?
+    Customer.where(query_params).to_json
+  end
 
   post :new, :provides => [:json] do
     @customer = Customer.new(parse_json(request))
+    
     if @customer.save
       return @customer.to_json
     else
