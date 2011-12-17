9fae01f7869901c0c4fe48a4762f597b5a6dd247
Jon Card <jon.card@gmail.com>
2011-12-16 22:22:10 -0700

    Adding premiums to auction profile.

--- app/models/config/profile.rb
+++ app/models/config/profile.rb
@@ -1,8 +1,13 @@
 class Profile
   include MongoMapper::Document
 
+  PREMIUM_TYPES = ["FIXED", "PERCENT"]
+  
   # Keys
-  key :name,             String
+  key :name,             String, :required => true
+  key :premium_type,     String
+  key :premium_amount,   Integer
+  key :is_default,       Boolean
 
   # Validations
   validates_uniqueness_of   :name,    :case_sensitive => false
